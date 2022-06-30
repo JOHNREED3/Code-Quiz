826cda56c61d4aa234f21540312b73491d79b7be
John Reed <87314038+JOHNREED3@users.noreply.github.com>
2022-06-30 15:51:53 -0400

    eod

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 var timerEl = document.getElementById("countdown");
 var highscoreEl = document.getElementById("highscore");
+var submitbtnEl = document.querySelector("#start-quiz");
 
+submitbtnEl.addEventListener("click", function () {
+	location.href = "./questions.html";
+});
